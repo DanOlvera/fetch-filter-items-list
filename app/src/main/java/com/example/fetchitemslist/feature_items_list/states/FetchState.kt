1183b7acf37e56c7d0ad67a4e13e1c3143054358
Dan Olvera <ing.josuedanielescamilla@gmail.com>
2025-02-20 20:12:05 -0800

    Renamed status to accurately represent the purpose

--- app/src/main/java/com/example/fetchitemslist/feature_items_list/states/FetchState.kt
+++
@@ -1,9 +0,0 @@
-package com.example.fetchitemslist.feature_items_list.states
-
-import com.example.fetchitemslist.feature_items_list.model.data.FetchItemsResponse
-
-sealed class FetchState {
-    data object Loading: FetchState()
-    data class Success(val data: FetchItemsResponse): FetchState()
-    data class Error(val message: String): FetchState()
-}
---
+++ app/src/main/java/com/example/fetchitemslist/feature_items_list/states/FetchUiState.kt
@@ -0,0 +1,9 @@
+package com.example.fetchitemslist.feature_items_list.states
+
+import com.example.fetchitemslist.feature_items_list.model.data.FetchItemsResponse
+
+sealed class FetchUiState {
+    data object Loading: FetchUiState()
+    data class Success(val data: FetchItemsResponse): FetchUiState()
+    data class Error(val message: String): FetchUiState()
+}
