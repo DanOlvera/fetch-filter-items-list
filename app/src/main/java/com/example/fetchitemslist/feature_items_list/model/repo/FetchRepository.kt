ca0ebe68a09c22fa25a1eeadf667f75414f663e0
Dan Olvera <ing.josuedanielescamilla@gmail.com>
2025-02-20 20:21:42 -0800

    Completed fetch repository

--- app/src/main/java/com/example/fetchitemslist/feature_items_list/model/repo/FetchRepository.kt
+++ app/src/main/java/com/example/fetchitemslist/feature_items_list/model/repo/FetchRepository.kt
@@ -1,9 +1,19 @@
 package com.example.fetchitemslist.feature_items_list.model.repo
 
 import com.example.fetchitemslist.feature_items_list.model.network.FetchApiService
+import com.example.fetchitemslist.feature_items_list.states.FetchUiState
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 
 class FetchRepository(private val apiService: FetchApiService) {
 
-    fun getFetch(): Flow<>
+    fun getFetch(): Flow<FetchUiState> = flow {
+        try {
+            emit(FetchUiState.Loading)
+            val response = apiService.getItems()
+            emit(FetchUiState.Success(response))
+        } catch (e: Exception) {
+            emit(FetchUiState.Error(e.message ?: "Unknown Error"))
+        }
+    }
 }
