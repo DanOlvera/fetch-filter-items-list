c3844581c793d9cbc001b3e78752720b0a14c2b4
Dan Olvera <ing.josuedanielescamilla@gmail.com>
2025-02-20 20:07:30 -0800

    Renamed model data class

--- app/src/main/java/com/example/fetchitemslist/feature_items_list/model/data/FetchItemsResponse.kt
+++ app/src/main/java/com/example/fetchitemslist/feature_items_list/model/data/FetchItemsResponse.kt
@@ -2,7 +2,7 @@ package com.example.fetchitemslist.feature_items_list.model.data
 
 import com.google.gson.annotations.SerializedName
 
-data class FetchItems (
+data class FetchItemsResponse (
 
     @SerializedName("id"     ) var id     : Int?    = null,
     @SerializedName("listId" ) var listId : Int?    = null,
